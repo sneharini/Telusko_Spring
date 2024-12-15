02d619b1334fd4a2de8b0d79c6f3be85176a8c64
sneharini <logumagisneharini@gmail.com>
2024-12-16 00:03:15 +0530

    genericsss

---
+++ src/Generic/Main.java
@@ -0,0 +1,19 @@
+package Generic;
+
+public class Main {
+    public static void main(String[] args) {
+//        Pair p = new Pair();
+//        p.first = "Scaler";
+//        p.second = 100;
+//
+//
+//        String city = p.second;// error - since second has integer but expected string- it gives RTE
+
+        Pair<String, Integer> p = new Pair<>();
+        p.first = "India";
+        p.second = 2000;
+
+        Pair<Integer, Double> p1 = new Pair<>();
+
+    }
+}
---
+++ src/Generic/Pair.java
@@ -0,0 +1,16 @@
+package Generic;
+
+public class Pair<T, U> {
+    T first;
+    U second;
+
+    T getFirst(){
+        return this.first;
+    }
+
+    void setFirst(T x){
+        this.first = x;
+
+
+    }
+}
