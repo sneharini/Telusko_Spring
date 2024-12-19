d60ecc570d67643b58c88b57011bd6264103fbd7
sneharini <logumagisneharini@gmail.com>
2024-12-19 21:42:03 +0530

    Exceptions

---
+++ src/Exceptions/Client.java
@@ -0,0 +1,34 @@
+package Exceptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            System.out.println(x / y);
+
+            File file = new File("filename");
+            FileReader fileReader = new FileReader(file);
+            fileReader.read();
+        }
+        catch (ArithmeticException e){
+            System.out.println("Got Arithmetic Exception");
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Got FileNotFoundException");
+        }
+        catch(Exception e){
+            System.out.println("Got general exception");
+        }
+
+        System.out.println("After all the catch blocks");
+    }
+}
